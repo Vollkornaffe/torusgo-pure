fe47ce81b905c7850fb7e4280b8f238e295f8349
Lars Helge Scheel <larshelge.scheel@gmail.com>
2017-08-26 19:55:11 +0200

    Impl. group function, sloppy testing

--- src/GameLogic.purs
+++ src/GameLogic.purs
@@ -3,9 +3,10 @@ module GameLogic where
 import Prelude
 import Data.Maybe (Maybe (Nothing, Just), fromJust)
 import Data.Tuple (Tuple (Tuple), fst, snd)
-import Data.Array (range, (!!), concatMap, snoc, updateAt)
+import Data.Array (range, (!!), concatMap, snoc, updateAt, filter, length, fromFoldable)
 import Data.String (fromCharArray)
 import Partial.Unsafe (unsafePartial)
+import Data.Set as Set
 
 data Color = Black | White
 type Position = Tuple Int Int
@@ -18,9 +19,11 @@ data Move = Pass | Play Position Field
 
 newtype State = State { board :: Board, size :: Size, moveNum :: Int, curCol :: Color }
 
+derive instance eqColor :: Eq Color
+
 init :: Size -> State
 init (Tuple w h) = 
-    let newBoard = map (\_ -> Nothing) (range 0 (w*h))
+    let newBoard = map (\_ -> Nothing) (range 0 (w*h-1))
     in State {
         board: newBoard,
         size: Tuple w h, 
@@ -28,13 +31,27 @@ init (Tuple w h) =
         curCol: Black
     }
 
+fromInts :: Size -> Array Int -> State
+fromInts size as =
+    let board = map field as
+    in State {
+        board: board,
+        size: size,
+        moveNum: 0,
+        curCol: Black
+    }
+    where field i = case i of
+            1 -> Just Black
+            2 -> Just White
+            _ -> Nothing
+
 canonPos :: Size -> Position -> Int
 canonPos (Tuple w h) (Tuple x y) =
     let x_mod = mod x w
         y_mod = mod y h
         x_mod_abs = if x_mod >= 0 then x_mod else w + x_mod
         y_mod_abs = if y_mod >= 0 then y_mod else w + y_mod
-    in y_mod_abs + w * y_mod_abs
+    in x_mod_abs + w * y_mod_abs
 
 getField :: State -> Position -> Field
 getField (State s) p = 
@@ -47,18 +64,58 @@ setField (State s) field p =
         board' = unsafePartial (fromJust (updateAt idx field s.board))
     in State (s { board = board' })
 
+neighPos :: Position -> Array Position
+neighPos (Tuple x y) = 
+    [ Tuple x (y+1),
+      Tuple (x+1) y,
+      Tuple x (y-1),
+      Tuple (x-1) y ]
+
+neighFields :: State -> Position -> Array Field
+neighFields state p = map (getField state) (neighPos p)
+
+neighFriends :: State -> Position -> Array Position
+neighFriends s p = 
+    case getField s p of
+        Nothing -> []
+        Just color -> filter (\n -> getField s n == Just color) (neighPos p)
+
+group :: State -> Position -> Array Position
+group s p =
+    case getField s p of
+        Nothing -> []
+        Just color -> fromFoldable (step (Set.singleton p) (Set.singleton p))
+    where
+        step mem newMem =
+            let newMemArray   = fromFoldable newMem
+                newNeighArray = concatMap (neighFriends s) newMemArray
+                newNeighSet   = Set.fromFoldable newNeighArray
+                newMem'       = Set.difference newNeighSet mem
+                mem'          = Set.union mem newMem'
+            in if Set.isEmpty newMem'
+                then mem
+                else step mem' newMem'
 
 toString :: State -> String
 toString state@(State s) = 
     let field Nothing = 'O'
         field (Just Black) = 'X'
         field (Just White) = 'Y'
-        row y = map (\x -> field (getField state (Tuple x y))) (range 0 (fst s.size))
-    in fromCharArray (concatMap (\y -> snoc (row y) '\n') (range 0 (snd s.size)))
+        row y = map (\x -> field (getField state (Tuple x y))) (range 0 ((fst s.size)-1))
+    in fromCharArray (concatMap (\y -> snoc (row y) '\n') (range 0 ((snd s.size)-1)))
 
 flipColor :: Color -> Color
 flipColor Black = White
 flipColor White = Black
+
+directLiberties :: State -> Position -> Int
+directLiberties s p = length ((filter ((==) Nothing)) (neighFields s p))
+
+--liberties :: State -> Position -> Maybe Int
+--liberties s p =
+    --case getField s p of 
+        --Nothing -> Nothing
+        --Just color -> 
 
 makeMove :: State -> Move -> Maybe State
 makeMove (State s) Pass = Just (State s { moveNum = s.moveNum + 1, curCol = flipColor s.curCol })
--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,24 @@
 module Main where
 
-import Prelude (Unit)
+import Prelude (Unit, discard, show)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-import GameLogic (init, toString)
+import GameLogic (State (State), init, toString, fromInts, group, canonPos)
 import Data.Tuple (Tuple (Tuple))
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
     let state = init (Tuple 3 3)
-    log (toString state)
+    let state'@(State s) = fromInts (Tuple 9 9) [ 0,0,0,0,0,0,0,0,0,
+                                                  0,0,0,0,0,0,0,0,0,
+                                                  0,0,0,2,2,0,0,0,0,
+                                                  0,0,0,0,2,2,2,1,2,
+                                                  0,0,0,0,0,0,2,1,2,
+                                                  1,1,1,0,0,0,2,2,1,
+                                                  0,0,0,0,0,0,0,2,0,
+                                                  0,0,0,0,0,0,0,0,0,
+                                                  0,0,0,0,0,0,0,0,0 ] 
+    log (toString state')
+    log (show (group state' (Tuple 0 5)))
+    log (show (canonPos s.size (Tuple 0 5)))
