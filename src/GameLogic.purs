0adfe859c2127e6ceaceb72ff1cb02c67eb89031
Lars Helge Scheel <larshelge.scheel@gmail.com>
2017-08-26 18:21:11 +0200

    change index calculations, handle negative x,y

--- src/GameLogic.purs
+++ src/GameLogic.purs
@@ -14,7 +14,9 @@ type Field = Maybe Color
 
 type Board = Array Field
 
-newtype State = State { board :: Board, size :: Size, moveNum :: Int, currentColor :: Color }
+data Move = Pass | Play Position Field
+
+newtype State = State { board :: Board, size :: Size, moveNum :: Int, curCol :: Color }
 
 init :: Size -> State
 init (Tuple w h) = 
@@ -23,23 +25,28 @@ init (Tuple w h) =
         board: newBoard,
         size: Tuple w h, 
         moveNum: 0,
-        currentColor: Black
+        curCol: Black
     }
 
+canonPos :: Size -> Position -> Int
+canonPos (Tuple w h) (Tuple x y) =
+    let x_mod = mod x w
+        y_mod = mod y h
+        x_mod_abs = if x_mod >= 0 then x_mod else w + x_mod
+        y_mod_abs = if y_mod >= 0 then y_mod else w + y_mod
+    in y_mod_abs + w * y_mod_abs
+
 getField :: State -> Position -> Field
-getField (State s) (Tuple x y) = 
-    let x_mod = mod x (fst s.size)
-        y_mod = mod y (snd s.size)                                                
-        idx = x_mod + fst s.size * y_mod                                          
+getField (State s) p = 
+    let idx = canonPos s.size p
     in unsafePartial (fromJust (s.board !! idx))
 
 setField :: State -> Field -> Position -> State
-setField (State s) field (Tuple x y) = 
-    let x_mod = mod x (fst s.size)
-        y_mod = mod y (snd s.size)
-        idx = x_mod + fst s.size * y_mod
+setField (State s) field p = 
+    let idx = canonPos s.size p
         board' = unsafePartial (fromJust (updateAt idx field s.board))
     in State (s { board = board' })
+
 
 toString :: State -> String
 toString state@(State s) = 
@@ -48,3 +55,11 @@ toString state@(State s) =
         field (Just White) = 'Y'
         row y = map (\x -> field (getField state (Tuple x y))) (range 0 (fst s.size))
     in fromCharArray (concatMap (\y -> snoc (row y) '\n') (range 0 (snd s.size)))
+
+flipColor :: Color -> Color
+flipColor Black = White
+flipColor White = Black
+
+makeMove :: State -> Move -> Maybe State
+makeMove (State s) Pass = Just (State s { moveNum = s.moveNum + 1, curCol = flipColor s.curCol })
+makeMove state (Play p f) = makeMove (setField state f p) Pass
