9f2a6e227e9a00bc1db05100b8c205cd6006bc62
Lukas Schneider <lukas.schneider@rwth-aachen.de>
2017-11-19 20:23:05 +0100

    Added logic directory for the game logic api

---
+++ logic/index.js
@@ -0,0 +1,37 @@
+import GameLogic from './lib/GameLogic';
+import {Tuple} from './lib/Data.Tuple';
+import Maybe from './lib/Data.Maybe';
+
+function Black () {
+  return new GameLogic.Black
+}
+
+function White () {
+  return new GameLogic.White
+}
+
+function init(x, y) {
+  let t = new Tuple(x,y);
+
+  return GameLogic.init(t);
+}
+
+function makeMove(state, x,y,color) {
+  let t = new Tuple(x,y);
+  let play = new GameLogic.Play(t, new Maybe.Just(color));
+
+  let maybe = GameLogic.makeMove(state)(play);
+
+  if(maybe instanceof Maybe.Just) {
+    return maybe.value0;
+  } else {
+    return null;
+  }
+}
+
+export default {
+  init,
+  makeMove,
+  Black,
+  White
+};
