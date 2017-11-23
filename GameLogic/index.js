ce9e592563361c53646bf526891b05af48e57d1f
Lukas Schneider <lukas.schneider@rwth-aachen.de>
2017-11-23 17:24:46 +0100

    - switched to require syntax to support node debugging - Just handling - Removed need for WHITE, BLACK - bug fixes

--- GameLogic/index.js
+++ GameLogic/index.js
@@ -1,95 +1,54 @@
-import autoBind from 'auto-bind';
-import GameLogic, {Black, White} from './output/GameLogic';
-import {Tuple} from './output/Data.Tuple';
-import Maybe, {Nothing} from './output/Data.Maybe';
+const autoBind = require('auto-bind');
+const GameLogic = require('./output/GameLogic');
+const DataTuple = require('./output/Data.Tuple');
+const DataMaybe = require('./output/Data.Maybe');
 
-const BLACK = imbueField(new Black());
-const WHITE = imbueField(new White());
-const PASS = new GameLogic.Pass();
+const {Black, White, Pass} = GameLogic;
+const {Tuple} = DataTuple;
+const {Nothing, Just} = DataMaybe;
 
-function imbueField(field) {
-  let color = (field instanceof Black || field instanceof White);
-  let black = (field instanceof Black);
-
-
-  field.isWhite = function () {
-    return !black && color;
-  };
-
-  field.isBlack = function () {
-    return black;
-  };
-
-  field.isEmpty = function() {
-    return !color;
-  };
-
-  field.equals = function(f) {
-    if (field instanceof Black && f instanceof Black) {
-      return true;
-    }
-    if (field instanceof White && f instanceof White) {
-      return true;
-    }
-    return field instanceof Nothing && f instanceof Nothing;
-  };
-
-  field.toString = function () {
-    return !color ? '-' : black ? 'b' : 'w';
-  };
-}
-
-function imbueSize(s) {
-  s.x = s.value0;
-  s.y = s.value1;
-  s.toString = function () {
-    return `${s.x}x${s.y}`;
-  };
-  return s;
-}
-
-function imbuePosition(p) {
-
-  p.x = p.value0;
-  p.y = p.value1;
-  p.toString = function () {
-    return `(${p.x}, ${p.y})`;
-  };
-  return p;
-}
 
 class State {
+
+  _state = {
+    size: null,
+    moveNum: 0,
+    koPos: null,
+    bPrison: 0,
+    wPrison: 0,
+    board: []
+  };
+
   constructor(x, y) {
-    let size = new Tuple(x,y);
-    this.setState(GameLogic.init(size));
+    let size = new Tuple(x, y);
+    this._setState(GameLogic.init(size));
 
     autoBind(this);
   }
 
-  setState(state ) {
+  _setState(state) {
     this._state = state;
     imbueSize(this._state.size);
-    imbuePosition(this._state.koPos); //todo handle maybe
+    imbuePosition(this._state.koPos);
     imbueField(this._state.curCol);
-    this._state.board.forEach((field) => {
-      imbueField(field);
-    });
+
+    this._state.board = this._state.board.map((field) => (imbueField(field)));
 
     return this;
   }
 
-  makeMove(x,y) {
-    let position = new Tuple(x,y);
+  makeMove(x, y) {
+    let position = new Tuple(x, y);
     let play = new GameLogic.Play(position);
-    return this.setState(GameLogic.makeMove(this._state)(play));
+    return this._setState(GameLogic.makeMove(this._state)(play));
   }
 
   pass() {
-    return this.setState(GameLogic.makeMove(this._state)(PASS));
+    return this._setState(GameLogic.makeMove(this._state)(new Pass()));
   }
 
-  testLegal(x,y) {
-    let position = new Tuple(x,y);
+  testLegal(x, y) {
+    let position = new Tuple(x, y);
     return GameLogic.testLegal(this._state)(position);
   }
 
@@ -97,60 +56,54 @@ class State {
     return this._state.size;
   }
 
-  getMove() {
+  getMoveNumber() {
     return this._state.moveNum;
   }
 
   getKoPosition() {
     return this._state.koPos;
-    //TODO handle maybe
   }
 
   getPrisoners(color) {
-    if(color.isBlack()) {
+    if (color.isBlack()) {
       return this._state.bPrison;
-    } else if(color.isWhite()){
+    } else if (color.isWhite()) {
       return this._state.wPrison;
     } else {
       return 0;
     }
   }
 
+  getBlackPrisoners() {
+    return this._state.bPrison;
+  }
+
+  getWhitePrisoners() {
+    return this._state.wPrison;
+  }
+
   getMovingColor() {
     return this._state.curCol;
   }
 
-  getField(x,y) {
-    let position = new Tuple(x,y);
-    let field = GameLogic.getField(this._state)(position);
-    return Maybe.fromMaybe(field);
+  getField(x, y) {
+    let position = new Tuple(x, y);
+    let canonicalPosition = GameLogic.canonPos(this.getSize())(position);
+    return this.getFieldArray()[canonicalPosition];
   }
 
   getFieldArray() {
     return this._state.board;
   }
 
-  get2dFieldArray() {
-    let size = this.getSize();
-
-    let array2d = [];
-    for(let i = 0; i < size.x; i++) {
-      array2d.push([]);
-      for(let j = 0; j < size.y; j++) {
-        array2d[i].push(this.getField(i,j));
-      }
-    }
-    return array2d;
-  }
-
   toString() {
     let size = this.getSize();
-    let move = this.getMove();
+    let move = this.getMoveNumber();
     let moving = this.getMovingColor();
     let ko = this.getKoPosition();
-    let bPrison = this.getPrisoners(BLACK);
-    let wPrison = this.getPrisoners(WHITE);
-    let board = this.get2dFieldArray();
+    let bPrison = this.getWhitePrisoners();
+    let wPrison = this.getBlackPrisoners();
+    let board = this.getFieldArray();
     return `State: { 
   size: ${size}, 
   moves: ${move}, 
@@ -163,9 +116,70 @@ class State {
   }
 }
 
-export default {
-  BLACK,
-  WHITE,
-  PASS,
-  State
+function imbueField(field) {
+  if (field instanceof Just) {
+    field = field.value0;
+  }
+
+  let color = (field instanceof Black || field instanceof White);
+  let black = (field instanceof Black);
+
+
+  field.isWhite = function () {
+    return !black && color;
+  };
+
+  field.isBlack = function () {
+    return black;
+  };
+
+  field.isEmpty = function () {
+    return !color;
+  };
+
+  field.equals = function (f) {
+    if (field instanceof Black && f instanceof Black) {
+      return true;
+    }
+    if (field instanceof White && f instanceof White) {
+      return true;
+    }
+    return field instanceof Nothing && f instanceof Nothing;
+  };
+
+  field.toString = function () {
+    return !color ? '-' : black ? 'b' : 'w';
+  };
+
+  return field;
+}
+
+function imbueSize(s) {
+  if (s instanceof Just) {
+    s = s.value0;
+  }
+
+  s.x = s.value0;
+  s.y = s.value1;
+
+  s.toString = function () {
+    return `${s.x}x${s.y}`;
+  };
+  return s;
+}
+
+function imbuePosition(p) {
+  if (p instanceof Just) {
+    p = p.value0;
+  }
+  p.x = p.value0;
+  p.y = p.value1;
+  p.toString = function () {
+    return `(${p.x}, ${p.y})`;
+  };
+  return p;
+}
+
+module.exports = {
+  State, White, Black, Nothing
 };
