6abcba0cfb53a4f121f31663297e1cb4f101ae9e
Lars Helge Scheel <larshelge.scheel@gmail.com>
2018-01-06 15:34:28 +0100

    Switching from tabs to spaces ^^

--- frontend/src/geometry/CustomTorusGeometry.js
+++ frontend/src/geometry/CustomTorusGeometry.js
@@ -27,125 +27,125 @@ class CustomTorusGeometry extends Geometry {
     this.updateGeometry();
   }
   
-	/**
-	 * creates connectivity (faces and quadFaces)
- 	 */
-	initGeometry() {
+  /**
+   * creates connectivity (faces and quadFaces)
+   */
+  initGeometry() {
 
-		// vertex id
-		let vId = 0;
+    // vertex id
+    let vId = 0;
 
-		// for conveniece
-		let x_seg = this.parameters.XSegments;
-		let y_seg = this.parameters.YSegments;
+    // for conveniece
+    let x_seg = this.parameters.XSegments;
+    let y_seg = this.parameters.YSegments;
 
-		for (let i = 0; i < x_seg; i++) {
-			for (let j = 0; j < y_seg; j++) {
+    for (let i = 0; i < x_seg; i++) {
+      for (let j = 0; j < y_seg; j++) {
 
-				// generate faces and quadFaces
-				if (i !== 0 && j !== 0) {
-					this.faces.push(new Face3(
-						vId - 1, vId, vId - y_seg
-					));
-					this.faces.push(new Face3(
-						vId - 1, vId - y_seg, vId - 1 - y_seg
-					));
-					this.quadFaces[vId] = [
-						vId, vId - y_seg, vId - 1 - y_seg, vId - 1
-					];
+        // generate faces and quadFaces
+        if (i !== 0 && j !== 0) {
+          this.faces.push(new Face3(
+            vId - 1, vId, vId - y_seg
+          ));
+          this.faces.push(new Face3(
+            vId - 1, vId - y_seg, vId - 1 - y_seg
+          ));
+          this.quadFaces[vId] = [
+            vId, vId - y_seg, vId - 1 - y_seg, vId - 1
+          ];
 
-					if (j === y_seg - 1) {
-						this.faces.push(new Face3(
-							vId, vId - y_seg + 1, vId - y_seg - y_seg + 1
-						));
-						this.faces.push(new Face3(
-							vId, vId - y_seg - y_seg + 1, vId - y_seg
-						));
-						this.quadFaces[vId - y_seg + 1] = [
-							vId, vId - y_seg + 1, vId - y_seg - y_seg + 1, vId - y_seg
-						];
-					}
-					if (i === x_seg - 1) {
-						this.faces.push(new Face3(
-							j - 1, j, vId
-						));
-						this.faces.push(new Face3(
-							j - 1, vId, vId - 1
-						));
-						this.quadFaces[j] = [
-							j, j - 1, vId, vId - 1
-						];
-					}
-					if (i === x_seg - 1 && j === y_seg - 1) {
-						this.faces.push(new Face3(
-							j, 0, vId - y_seg + 1
-						));
-						this.faces.push(new Face3(
-							j, vId - y_seg + 1, vId
-						));
-						this.quadFaces[0] = [
-							0, j, vId - y_seg + 1, vId
-						];
-					}
-				}
+          if (j === y_seg - 1) {
+            this.faces.push(new Face3(
+              vId, vId - y_seg + 1, vId - y_seg - y_seg + 1
+            ));
+            this.faces.push(new Face3(
+              vId, vId - y_seg - y_seg + 1, vId - y_seg
+            ));
+            this.quadFaces[vId - y_seg + 1] = [
+              vId, vId - y_seg + 1, vId - y_seg - y_seg + 1, vId - y_seg
+            ];
+          }
+          if (i === x_seg - 1) {
+            this.faces.push(new Face3(
+              j - 1, j, vId
+            ));
+            this.faces.push(new Face3(
+              j - 1, vId, vId - 1
+            ));
+            this.quadFaces[j] = [
+              j, j - 1, vId, vId - 1
+            ];
+          }
+          if (i === x_seg - 1 && j === y_seg - 1) {
+            this.faces.push(new Face3(
+              j, 0, vId - y_seg + 1
+            ));
+            this.faces.push(new Face3(
+              j, vId - y_seg + 1, vId
+            ));
+            this.quadFaces[0] = [
+              0, j, vId - y_seg + 1, vId
+            ];
+          }
+        }
 
-				vId++;
+        vId++;
 
-			}
-		}
+      }
+    }
 
-		this.dynamic = true;
-	}
+    this.dynamic = true;
+  }
 
-	/**
-	 * called for initialization and
-	 * should be called whenever the twist parameter is changed
-	 */
-	updateGeometry = function () {
+  /**
+   * called for initialization and
+   * should be called whenever the twist parameter is changed
+   */
+  updateGeometry = function () {
 
-		// for conviniece
-		let x_seg = this.parameters.XSegments;
-		let y_seg = this.parameters.YSegments;
+    // for conviniece
+    let x_seg = this.parameters.XSegments;
+    let y_seg = this.parameters.YSegments;
 
-		// axis
-		let x_ax = new Vector3(1,0,0);
-		let y_ax = new Vector3(0,1,0);
+    // axis
+    let x_ax = new Vector3(1,0,0);
+    let y_ax = new Vector3(0,1,0);
 
-		for (let i = 0; i < x_seg; i++) {
+    for (let i = 0; i < x_seg; i++) {
 
-			// this is where the TWIST has an influence!
-			let i_rad = i / x_seg * 2 * Math.PI + this.parameters.twist;
+      // this is where the TWIST has an influence!
+      let i_rad = i / x_seg * 2 * Math.PI + this.parameters.twist;
 
-			// first we compute the 'offsets', meaning one of many identical 'ring' segments
-			let offset = new Vector3(
-				this.parameters.thickness * Math.cos(i_rad),
-				this.parameters.thickness * Math.sin(i_rad),
-				0
-			);
+      // first we compute the 'offsets', meaning one of many identical 'ring' segments
+      let offset = new Vector3(
+        this.parameters.thickness * Math.cos(i_rad),
+        this.parameters.thickness * Math.sin(i_rad),
+        0
+      );
 
-			// now we can spin the ring to form a complete torus!
-			for (let j = 0; j < y_seg; j++) {
-				let j_rad = j / x_seg * 2 * Math.PI;
+      // now we can spin the ring to form a complete torus!
+      for (let j = 0; j < y_seg; j++) {
+        let j_rad = j / x_seg * 2 * Math.PI;
 
-				// individual vertex positions
-				let newPos = new Vector3();
+        // individual vertex positions
+        let newPos = new Vector3();
 
-				// first copy the ring position
-				newPos.copy(offset);
+        // first copy the ring position
+        newPos.copy(offset);
 
-				// then put it at the disired radius, in y-direction
-				newPos.addScaledVector(y_ax, this.parameters.radius);
+        // then put it at the disired radius, in y-direction
+        newPos.addScaledVector(y_ax, this.parameters.radius);
 
-				// and rotate it around the x-axis to get to the final position
-				newPos.applyAxisAngle(x_ax, j_rad);
+        // and rotate it around the x-axis to get to the final position
+        newPos.applyAxisAngle(x_ax, j_rad);
 
-				this.vertices.push(newPos);
-			}
-		}
+        this.vertices.push(newPos);
+      }
+    }
 
-		this.verticesNeedUpdate = true;
-		this.computeVertexNormals();
-	}
+    this.verticesNeedUpdate = true;
+    this.computeVertexNormals();
+  }
 }
 
 export default CustomTorusGeometry;
