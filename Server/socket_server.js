b29be8e3776f0836f0ea5c5e571219311e7f2eb6
Lars Scheel <lars.helge.scheel@rwth-aachen.de>
2018-04-02 21:57:03 +0200

    CreateConnection works out, pool doesn't... I guess we can just use that then

--- Server/socket_server.js
+++ Server/socket_server.js
@@ -2,34 +2,40 @@ let SOCKET_PORT = process.env.SOCKET_PORT;
 
 let https = require('https');
 let fs    = require('fs');
-let sanitizer = require('sanitizer');
-
-let mysql = require('promise-mysql');
-let db_pool = mysql.createPool({
-    host     : 'example.org',
-    user     : 'bob',
-    password : 'secret',
-    database : 'my_db'
-});
 
 let app = https.createServer({
     key:    fs.readFileSync('/home/vollkorn/.config/letsencrypt/live/torusgo.com/privkey.pem'),
     cert:   fs.readFileSync('/home/vollkorn/.config/letsencrypt/live/torusgo.com/fullchain.pem'),
     ca:     fs.readFileSync('/home/vollkorn/.config/letsencrypt/live/torusgo.com/chain.pem')
 });
+
+let sanitizer = require('sanitizer');
+
+let mysql = require('promise-mysql');
+let db_credentials = {
+    host     : 'example.org',
+    user     : 'bob',
+    password : 'secret',
+    database : 'my_db'
+};
+
 let io = require('socket.io').listen(app);
 
 let register_user = (accountData, socket) => {
+  let connection;
   const {username, password, email} = accountData;
-  // first check whether username or email is duplicate
-  let sql_check_dup = "FROM users SELECT 1 WHERE username = ? OR email = ?";
-  sql_check_dup = mysql.format(sql_check_dup, [username, email]);
-  db_pool.query(sql_check_dup)
-    .then((rows) => {
-      if (rows.length !== 0) {
-        if (rows[0].username === username) throw {name: "FeedbackError", message: "Username exists."};
-        if (rows[0].email === email) throw {name: "FeedbackError", message: "Email exists."};
-        throw {name: "FeedbackError", message: "Account creation failed, Error 0"};
+  mysql.createConnection(db_credentials)
+    .then((conn) => { 
+      connection = conn;
+    })
+    .then(() => {
+      // first check whether username or email is duplicate
+      let sql = "FROM users SELECT 1 WHERE username = ? OR email = ?";
+      sql = mysql.format(sql, [username, email]);
+      let duplicates = connection.query(sql);
+      if (duplicates.length !== 0) {
+        if (duplicates[0].username === username) throw {name: "FeedbackError", message: "Username in use."};
+        if (duplicates[0].email === email) throw {name: "FeedbackError", message: "Email in use."};
       }
     })
     // TODO actually create account
@@ -40,6 +46,8 @@ let register_user = (accountData, socket) => {
       console.log(err);
       if (err.name === "FeedbackError") {
         socket.emit("failure", err.message);
+      } else {
+        socket.emit("failure", "DB error without feedback... try again later");
       }
     });
 };
