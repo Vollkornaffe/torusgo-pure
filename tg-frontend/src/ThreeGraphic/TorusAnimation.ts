dcee53e007b3c15747f5b2821397cfd6cb172c12
Lars Helge Scheel <larshelge.scheel@gmail.com>
2018-08-12 03:09:58 +0200

    Code is a mess, but experiment succ

--- tg-frontend/src/ThreeGraphic/TorusAnimation.ts
+++ tg-frontend/src/ThreeGraphic/TorusAnimation.ts
@@ -18,6 +18,7 @@ import {
   Scene,
   ShaderMaterial,
   TorusBufferGeometry,
+  Uniform,
   Vector2,
   Vector3,
   WebGLRenderer,
@@ -78,14 +79,16 @@ class TorusAnimation {
   private torusMaterialFaces: THREE.MeshPhongMaterial;
   private torusMaterialLines: THREE.LineBasicMaterial;
 
+	private angle: number;
   private experimentalShader: THREE.ShaderMaterial;
 
-  // private torusGeometry: THREE.BoxGeometry;
+  private testGeometry: THREE.BoxGeometry;
   private torusGeometryGeneral: TorusGeometryGeneral;
   private torusGeometryFaces: TorusGeometryFaces;
   private torusGeometryLines: TorusGeometryLines;
   // private torusGeometryRaycast: TorusGeometryRaycast;
 
+  private testMesh: THREE.Mesh;
   private torusMeshFaces: THREE.Mesh;
   private torusMeshLines: THREE.LineSegments;
   // private torusMeshRaycast: THREE.Mesh;
@@ -106,7 +109,7 @@ class TorusAnimation {
       CAMERA_FAR
     );
     this.scene.add(this.camera);
-    this.camera.position.set(5, 0, 0);
+    this.camera.position.set(0, 0, 5);
     this.camera.lookAt(ORIGIN.clone());
     this.ambientLight = new AmbientLight(LIGHT_COLOR_AMBIENT);
     this.directionalLight = new DirectionalLight(LIGHT_COLOR_DIRECTIONAL);
@@ -124,20 +127,146 @@ class TorusAnimation {
       linecap: 'round',
     });
 
+		this.angle = 0.0;
     this.experimentalShader = new ShaderMaterial({
+			uniforms: {
+        inverseViewMatrix: new Uniform(this.camera.matrixWorld),
+			},
     	vertexShader: [
         "void main() {",
           "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-        "}"
+        "}",
       ].join( "\n" ),
 
       fragmentShader: [
+        "uniform mat4 inverseViewMatrix;",
+
+"float iTorus( in vec3 ro, in vec3 rd, in vec2 torus )",
+"{",
+	"float Ra2 = torus.x*torus.x;",
+	"float ra2 = torus.y*torus.y;",
+	"",
+	"float m = dot(ro,ro);",
+	"float n = dot(ro,rd);",
+		"",
+	"float k = (m - ra2 - Ra2)/2.0;",
+	"float a = n;",
+	"float b = n*n + Ra2*rd.z*rd.z + k;",
+	"float c = k*n + Ra2*ro.z*rd.z;",
+	"float d = k*k + Ra2*ro.z*ro.z - Ra2*ra2;",
+	"",
+    "//----------------------------------",
+"",
+	"float p = -3.0*a*a     + 2.0*b;",
+	"float q =  2.0*a*a*a   - 2.0*a*b   + 2.0*c;",
+	"float r = -3.0*a*a*a*a + 4.0*a*a*b - 8.0*a*c + 4.0*d;",
+	"p /= 3.0;",
+	"r /= 3.0;",
+	"float Q = p*p + r;",
+	"float R = 3.0*r*p - p*p*p - q*q;",
+	"",
+	"float h = R*R - Q*Q*Q;",
+	"float z = 0.0;",
+	"if( h < 0.0 )",
+	"{",
+		"float sQ = sqrt(Q);",
+		"z = 2.0*sQ*cos( acos(R/(sQ*Q)) / 3.0 );",
+	"}",
+	"else",
+	"{",
+		"float sQ = pow( sqrt(h) + abs(R), 1.0/3.0 );",
+		"z = sign(R)*abs( sQ + Q/sQ );",
+"",
+	"}",
+	"",
+	"z = p - z;",
+	"",
+    "//----------------------------------",
+	"",
+	"float d1 = z   - 3.0*p;",
+	"float d2 = z*z - 3.0*r;",
+"",
+	"if( abs(d1)<1.0e-4 )",
+	"{",
+		"if( d2<0.0 ) return -1.0;",
+		"d2 = sqrt(d2);",
+	"}",
+	"else",
+	"{",
+		"if( d1<0.0 ) return -1.0;",
+		"d1 = sqrt( d1/2.0 );",
+		"d2 = q/d1;",
+	"}",
+"",
+    "//----------------------------------",
+	"",
+	"float result = 1e20;",
+"",
+	"h = d1*d1 - z + d2;",
+	"if( h>0.0 )",
+	"{",
+		"h = sqrt(h);",
+		"float t1 = -d1 - h - a;",
+		"float t2 = -d1 + h - a;",
+		     "if( t1>0.0 ) result=t1;",
+		"else if( t2>0.0 ) result=t2;",
+	"}",
+"",
+	"h = d1*d1 - z - d2;",
+	"if( h>0.0 )",
+	"{",
+		"h = sqrt(h);",
+		"float t1 = d1 - h - a;",
+		"float t2 = d1 + h - a;",
+		     "if( t1>0.0 ) result=min(result,t1);",
+		"else if( t2>0.0 ) result=min(result,t2);",
+	"}",
+"",
+	"return result;",
+"}",
+"",
+
+"// df(x)/dx",
+"vec3 nTorus( in vec3 pos, vec2 tor )",
+"{",
+	"return normalize( pos*(dot(pos,pos)- tor.y*tor.y - tor.x*tor.x*vec3(1.0,1.0,-1.0)));",
+"}",
+
+
         "void main() {",
-          "gl_FragColor = vec4( 1.0, 0.0, 0.0, 0.5 );",
-        "}"
+          "vec4 camera_wc = inverseViewMatrix * vec4(0.0,0.0,0.0,1.0);",
+          "vec4 imgplane_wc = inverseViewMatrix * vec4((2.0*gl_FragCoord.x - 1000.0)*0.001,(2.0*gl_FragCoord.y - 1000.0)*0.001,-1.0,1.0);",
+          "vec4 ray_wc = normalize(imgplane_wc - camera_wc);",
+
+	"// raytrace-plane",
+	"vec2 torus = vec2(2.0,0.9);",
+	"float t = iTorus( camera_wc.xyz, ray_wc.xyz, torus );",
+
+
+
+					"if (t < 0.0) {discard;}",
+  "// shading/lighting	",
+	"vec3 col = vec3(0.0);",
+	"if( t>0.0 && t<100.0 )",
+	"{",
+	    "vec3 pos = camera_wc.xyz + t*ray_wc.xyz;",
+		"vec3 nor = nTorus( pos, torus );",
+		"float dif = clamp( dot(nor,vec3(0.57703)), 0.0, 1.0 );",
+		"float amb = clamp( 0.5 + 0.5*dot(nor,vec3(0.0,1.0,0.0)), 0.0, 1.0 );",
+		"col = vec3(0.2,0.3,0.4)*amb + vec3(1.0,0.9,0.7)*dif;",
+		"col *= 0.8;",
+	"}",
+	"",
+	"col = sqrt( col );",
+
+          // "if (dot(ray_wc, camera_wc) * dot(ray_wc, camera_wc) - dot(camera_wc, camera_wc) + 2.0 < 0.0) {discard;}",
+
+					"gl_FragColor = vec4( col, 1.0 );",
+				"}",
       ].join( "\n" )
     });
 
+    this.testGeometry = new BoxGeometry(2,2,2);
     this.torusGeometryGeneral = new TorusGeometryGeneral(
       givenSetup.boardSizeX,
       givenSetup.boardSizeY,
@@ -150,15 +279,16 @@ class TorusAnimation {
     this.torusGeometryLines = new TorusGeometryLines(this.torusGeometryGeneral);
     // this.torusGeometryRaycast = new TorusGeometryRaycast(this.torusGeometryGeneral);
 
-    // this.torusMeshFaces = new Mesh(this.torusGeometryFaces, this.torusMaterialFaces);
-    this.torusMeshFaces = new Mesh(this.torusGeometryFaces, this.experimentalShader);
+    this.testMesh = new Mesh(this.testGeometry, this.experimentalShader);
+    this.torusMeshFaces = new Mesh(this.torusGeometryFaces, this.torusMaterialFaces);
     this.torusMeshLines = new LineSegments(this.torusGeometryLines, this.torusMaterialLines);
     // this.torusMeshRaycast = new Mesh(this.torusGeometryFancy, this.torusMaterialFancy);
 
     // this.torusMeshFancy.rotation.y = Math.PI / 2;
     // this.torusMeshFancy.geometry.computeVertexNormals();
 
-    this.scene.add(this.torusMeshFaces);
+    this.scene.add(this.testMesh);
+    // this.scene.add(this.torusMeshFaces);
     // this.scene.add(this.torusMeshLines);
     // this.scene.add(this.torusMeshRaycast);
 
@@ -186,6 +316,13 @@ class TorusAnimation {
     // this.updateRaycast();
 
     // End of animation code
+
+		// Use Math.cos and Math.sin to set camera X and Z values based on angle. 
+		this.camera.position.x = 5.0 * Math.cos( this.angle );  
+		this.camera.position.z = 5.0 * Math.sin( this.angle );
+		console.log(this.camera.position);
+		this.camera.lookAt(0.0,0.0,0.0);
+		this.angle += 0.01;
 
     this.torusGeometryGeneral.update();
     this.torusGeometryFaces.update();
