588b65d47234f87cbbbd4947afcadd9a6cca63c5
Lars Helge Scheel <larshelge.scheel@gmail.com>
2018-09-12 20:29:13 +0200

    Implemeted gamelog up to testLegal, gonna test now

--- tg-frontend/src/utils/game-logic.ts
+++ tg-frontend/src/utils/game-logic.ts
@@ -1,4 +1,5 @@
 import {EColor, EField, IGame, IPosition, IRawGame, IRuleSet, ISize, TGameBoard, TMove} from '../types/game';
+import {start} from "repl";
 
 export function testMove(game: IGame, move: TMove): boolean {
 
@@ -15,6 +16,7 @@ export function applyMove(game: Readonly<IGame>, move: TMove): IGame {
 }
 
 // here the port from purescript, maybe exported to a npm module.
+// everything is intended to be purely functional
 
 // init game with empty board
 // TODO handle handicap
@@ -53,6 +55,7 @@ function canonPos(size: ISize, pos: IPosition) : IPosition {
   return { x, y };
 }
 
+// I don't know whether this will ever be needed
 function indexToPos(size: ISize, idx: number) : IPosition {
   let remainder = idx;
   let x = 0;
@@ -68,12 +71,30 @@ function posToIndex(size: ISize, pos: IPosition) {
   return cPos.y + cPos.x * size.y;
 }
 
-function setField(board: TGameBoard, field: EField, idx: number) : TGameBoard {
+// just annoying cast to field
+function colorToField(color: EColor): EField{
+  switch (color) {
+    case EColor.White: return EField.White;
+    case EColor.Black: return EField.Black;
+  }
+}
+
+function getField(size: ISize, board: TGameBoard, pos: IPosition) {
+  return board[posToIndex(size, pos)];
+}
+
+// when this is called, the returned board is modified
+// remember that boards captured in filter functions etc.
+// will NOT be up to date then!
+function setField(size: ISize, board: TGameBoard, field: EField, pos: IPosition) : TGameBoard {
   const newBoard = [...board];
-  newBoard[idx] = field;
+  newBoard[posToIndex(size, pos)] = field;
   return newBoard;
 }
 
+// can be passed a filter
+// to only find positions that have a certain field value
+// or to make sure that no loops happen in depth searches
 function getNeighPosArray(
   pos: IPosition,
   posFilter = (potentialNeigh: IPosition) => true,
@@ -90,6 +111,9 @@ function flipColor(color: EColor) : EColor {
   return color === EColor.Black ? EColor.Black : EColor.White;
 }
 
+// there may be a better way for this
+// the intention is to remove duplicate positions
+// first get canonical positions, then only add if not present yet
 function canonAndRemoveDups(size: ISize, positions: IPosition[]) : IPosition[] {
   const canonPositions = positions.map((pos: IPosition) => {
     return canonPos(size, pos);
@@ -105,47 +129,122 @@ function canonAndRemoveDups(size: ISize, positions: IPosition[]) : IPosition[] {
   return noDups;
 }
 
-function getGroup(
+// this is the head for the recursive group search
+// can be passed a filter which will usually just check for correct field
+// THE STARTING POSITION WILL NOT BE FILTERED
+function getGroupWithFilter(
   size: ISize,
   board: TGameBoard,
+  filter: (pos: IPosition) => boolean,
   startingPos: IPosition,
-  field: EField,
 ): IPosition[] {
-  if (board[posToIndex(size, startingPos)] !== field) {
-    return [];
-  }
-  return getGroupRecursive(size, board, [], [canonPos(size, startingPos)]);
+  return getGroupWithFilterRecursive(size, board, filter, [], [canonPos(size, startingPos)]);
 }
 
 // members and newMembers always canonical
-function getGroupRecursive(
+function getGroupWithFilterRecursive(
   size: ISize,
   board: TGameBoard,
+  filter: (pos: IPosition) => boolean,
   members: IPosition[],
   newMembers: IPosition[],
 ) : IPosition[] {
-  if (newMembers === []) {
-    return members;
-  }
 
-  const membersNext = members.concat(newMembers);
+  // we don't want to add previously visited positions -> infinite loop
+  // also apply the passed filter
   const neighFilter = (potentialNeigh: IPosition): boolean => {
-    return membersNext.indexOf(canonPos(size, potentialNeigh)) === -1;
+    return membersNext.indexOf(canonPos(size, potentialNeigh)) === -1
+      && filter(potentialNeigh);
   };
+
+  // get the wanted unvisited neighbors of all newMembers
   const newMembersSameFieldNeighArrays = newMembers.map(
     (newMember: IPosition): IPosition[] => {
     return getNeighPosArray(newMember, neighFilter);
   });
+
+  // flatten result & remove duplicates
   const newMembersNextWithDups = [].concat.apply([], newMembersSameFieldNeighArrays);
   const newMembersNext = canonAndRemoveDups(size, newMembersNextWithDups);
 
-  return getGroupRecursive(size, board, membersNext, newMembersNext);
+  // previous newMembers are now done
+  const membersNext = members.concat(newMembers);
+
+  if (newMembersNext === []) {
+    // recursion stop nothing new was found
+    return members;
+  } else {
+    // next step in search
+    return getGroupWithFilterRecursive(size, board, filter, membersNext, newMembersNext);
+  }
 }
 
-function directLiberties(size: ISize, board: TGameBoard, pos: IPosition): number {
-  const freePositions = getNeighPosArray(pos, (potentialFree: IPosition) => {
-    return board[posToIndex(size, potentialFree)] === EField.Empty;
+// for a given position gets the positions of empty fields
+// adjecent to the connected group
+// only makes sense to call this with a color
+function groupEmptyPositions(size: ISize, board: TGameBoard, color: EColor, pos: IPosition): IPosition[] {
+
+  const field = colorToField(color);
+
+  // only same color ofc
+  const groupFilter = (pos: IPosition) => {
+    return getField(size, board, pos) === field;
+  };
+  const group = getGroupWithFilter(size, board, groupFilter, pos);
+
+  // get the adject empty neighbors as multiple arrays
+  const groupEmptyPositionsArrays = group.map((member: IPosition) => {
+    getNeighPosArray(member, (potentialEmpty: IPosition) => {
+      return getField(size, board, potentialEmpty) === EField.Empty;
+    });
   });
-  return freePositions.length;
+
+  // flatten result & remove duplicates
+  const groupEmptyPositionsWithDups = [].concat.apply([], groupEmptyPositionsArrays);
+  return canonAndRemoveDups(size, groupEmptyPositionsWithDups);
 }
+
+// tests the legality of the intended move without applying it
+function testLegal(
+  toMove: EColor,
+  size: ISize,
+  board: TGameBoard,
+  ko: boolean,
+  koPosition: IPosition,
+  intendedPos: IPosition,
+): boolean {
+
+  // can't play on non empty
+  if (getField(size, board, intendedPos) !== EField.Empty) {
+    return false;
+  }
+
+  // can't play in the ko
+  if(ko && canonPos(size, intendedPos) === koPosition) {
+    return false;
+  }
+
+  // maybe we capture some enemy stones then?
+  const enemyEmpty = groupEmptyPositions(size, board, flipColor(toMove), intendedPos);
+  if (enemyEmpty.length === 1) {
+    if (enemyEmpty.indexOf(canonPos(size, intendedPos)) > -1) {
+      return true;
+    }
+  }
+
+  // now we must look into the empty fields
+  return groupEmptyPositions(size, board, toMove, intendedPos).length > 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
